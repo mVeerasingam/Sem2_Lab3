0107cb7bba981aef6a07b48324ea8ea033aa76f6
Mark Veerasingam <g00403931@atu.ie>
2023-02-02 12:12:22 +0000

    completed tv class

--- src/main/java/ie/atu/productv4/TV.java
+++ src/main/java/ie/atu/productv4/TV.java
@@ -1,4 +1,34 @@
 package ie.atu.productv4;
 
-public class TV {
+public class TV extends Product{
+    private String tv;
+    private String brand;
+
+    public TV(){
+        super();
+        tv = "";
+        brand = "";
+        count++;
+    }
+
+    public String getTv() {
+        return tv;
+    }
+
+    public void setTv(String tv) {
+        this.tv = tv;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " by " + tv + "\nManufactured by " + brand;
+    }
 }
