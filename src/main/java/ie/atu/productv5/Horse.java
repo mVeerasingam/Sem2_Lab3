e7c5f63e94ac1a761d00c9e8d1a9e965f895f112
Zilvinovas <123949273+Zilvinovas@users.noreply.github.com>
2023-02-16 12:39:40 +0000

    Added Horse

---
+++ src/main/java/ie/atu/productv5/Horse.java
@@ -0,0 +1,48 @@
+package ie.atu.productv5;
+
+public class Horse extends Animal{
+
+    private String dam;
+    private String sire;
+    private String height;
+
+    public Horse() {
+        super();
+        this.dam = dam;
+        this.sire = sire;
+        this.height = height;
+    }
+
+    public String getDam() {
+        return dam;
+    }
+
+    public void setDam(String dam) {
+        this.dam = dam;
+    }
+
+    public String getSire() {
+        return sire;
+    }
+
+    public void setSire(String sire) {
+        this.sire = sire;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Horse{" +
+                "dam='" + dam + '\'' +
+                ", sire='" + sire + '\'' +
+                ", height='" + height + '\'' +
+                '}';
+    }
+}
